ca97e1565791b1a0102452147584c98a809f1054
jvb6debug <joao.v.barbosa6@ba.estudante.senai.br>
2026-05-20 12:38:27 -0300

    Delete routes directory

--- routes/loginRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const loginController = require('../controllers/loginController');
-const AuthMiddleware = require('../middleware/auth');
-
-const router = express.Router();
-
-router.post("/", AuthMiddleware.authenticateToken, loginController.login);
-
-module.exports = router;
